801afe7e365983001e356f17122564aeb8e22230
Chetan Titare <chetantitare58@gmail.com>
2026-02-02 21:17:39 +0530

    Remove extra Chetan_Project folder

--- Chetan_Project/c language/Control Statement/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
- fab=a+b;
-    for(int i=1; i<=n; i++){
-        fab=fab+i;
-        printf("%d, ",fab);
-    }
--- Chetan_Project/c language/Pattern/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-int end=1;
-        // for(int a=space-1; a<space; a++){
-        //     printf("%d",end);
-        //     end+=1;
-        // }
